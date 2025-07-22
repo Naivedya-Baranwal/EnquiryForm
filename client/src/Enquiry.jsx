1e275cb7591fbb65729158d833dfc8b9a5929d1d
Naivedya Baranwal <naivedyab198@gmail.com>
2025-07-22 16:29:28 +0530

    change in form

--- client/src/Enquiry.jsx
+++ client/src/Enquiry.jsx
@@ -6,7 +6,7 @@ const Enquiry = () => {
         <div>
             <h1 className='font-bold text-4xl text-center'>User Enquiry</h1>
             <div className='grid grid-cols-[30%_auto]'>
-                <div className='bg-gray-200 p-4'>
+                <div className='bg-gray-300 p-4'>
                     <h1 className='text-xl font-bold'>Enquiry Form</h1>
                     <form action="">
                        <div className='py-3 flex flex-col'>
